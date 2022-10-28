5be69b5584bd70800dd29af6816dcff962c4f270
influxion <nicholsjordon721@gmail.com>
2022-10-28 15:17:39 -0500

    Adjusted Tailwind screen sizes

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,19 @@ module.exports = {
     './src/components/**/*.{js,jsx,ts,tsx}',
   ],
   theme: {
+    screens: {
+      sm: { max: '544px' },
+      // => @media (max-width: 544px) { ... }
+
+      md: { max: '768px' },
+      // => @media (max-width: 768px) { ... }
+
+      lg: { max: '944px' },
+      // => @media (max-width: 944px) { ... }
+
+      xl: { max: '1200px' },
+      // => @media (max-width: 1200px) { ... }
+    },
     extend: {},
   },
   plugins: [],
